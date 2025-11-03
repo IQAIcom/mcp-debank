c2c626ea109c56e418b16447ce530bfe561ac08a
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-11-03 11:26:41 +0100

    feat: add chainIds enum with supported blockchain networks

---
+++ src/enums/chains.ts
@@ -0,0 +1,452 @@
+export const chainIds = [
+	{
+		id: "loot",
+		name: "Loot",
+		wrappedTokenId: "0x4fa214c9e33d481996bec77c443245fbaee85670",
+	},
+	{
+		id: "frax",
+		name: "Fraxtal",
+		wrappedTokenId: "0xfc00000000000000000000000000000000000006",
+	},
+	{
+		id: "step",
+		name: "Step",
+		wrappedTokenId: "0xb58a9d5920af6ac1a9522b0b10f55df16686d1b6",
+	},
+	{
+		id: "heco",
+		name: "HECO",
+		wrappedTokenId: "0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f",
+	},
+	{
+		id: "merlin",
+		name: "Merlin",
+		wrappedTokenId: "0xf6d226f9dc15d9bb51182815b320d3fbe324e1ba",
+	},
+	{
+		id: "wan",
+		name: "Wanchain",
+		wrappedTokenId: "0xdabd997ae5e4799be47d6e69d9431615cba28f48",
+	},
+	{
+		id: "mode",
+		name: "Mode",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "bfc",
+		name: "Bifrost",
+		wrappedTokenId: "0x1c1b06405058abe02e4748753aed1458befee3b9",
+	},
+	{
+		id: "wemix",
+		name: "WEMIX",
+		wrappedTokenId: "0x7d72b22a74a216af4a002a1095c8c707d6ec1c5f",
+	},
+	{
+		id: "rari",
+		name: "RARI",
+		wrappedTokenId: "0xf037540e51d71b2d2b1120e8432ba49f29edfbd0",
+	},
+	{
+		id: "avax",
+		name: "Avalanche",
+		wrappedTokenId: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+	},
+	{
+		id: "apex",
+		name: "Proof of Play Apex",
+		wrappedTokenId: "0x77684a04145a5924efce0d92a7c4a2a2e8c359de",
+	},
+	{
+		id: "fx",
+		name: "Function X",
+		wrappedTokenId: "0x80b5a32e4f032b2a058b4f29ec95eefeeb87adcd",
+	},
+	{
+		id: "pego",
+		name: "Pego",
+		wrappedTokenId: "0x0cf4071940782b640d0b595cb17bdf3e90869d70",
+	},
+	{
+		id: "fuse",
+		name: "Fuse",
+		wrappedTokenId: "0x0be9e53fd7edac9f859882afdda116645287c629",
+	},
+	{
+		id: "core",
+		name: "CORE",
+		wrappedTokenId: "0x40375c92d9faf44d2f9db9bd9ba41a3317a2404f",
+	},
+	{
+		id: "ftm",
+		name: "Fantom",
+		wrappedTokenId: "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83",
+	},
+	{
+		id: "pls",
+		name: "Pulse",
+		wrappedTokenId: "0xa1077a294dde1b09bb078844df40758a5d0f9a27",
+	},
+	{
+		id: "arb",
+		name: "Arbitrum",
+		wrappedTokenId: "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
+	},
+	{
+		id: "klay",
+		name: "Klaytn",
+		wrappedTokenId: "0xe4f05a66ec68b54a58b17c22107b02e0232cc817",
+	},
+	{
+		id: "rose",
+		name: "Oasis Emerald",
+		wrappedTokenId: "0x21c718c22d52d0f3a789b752d4c2fd5908a8a733",
+	},
+	{
+		id: "mnt",
+		name: "Mantle",
+		wrappedTokenId: "0x78c1b0c915c4faa5fffa6cabf0219da63d7f4cb8",
+	},
+	{
+		id: "btt",
+		name: "BitTorrent",
+		wrappedTokenId: "0x197a4ed2b1bb607e47a144b9731d7d34f86e9686",
+	},
+	{
+		id: "flr",
+		name: "Flare",
+		wrappedTokenId: "0x1d80c49bbbcd1c0911346656b529df9e5c2f783d",
+	},
+	{ id: "lyx", name: "LUKSO", wrappedTokenId: "" },
+	{ id: "xai", name: "Xai", wrappedTokenId: "" },
+	{
+		id: "mobm",
+		name: "Moonbeam",
+		wrappedTokenId: "0xacc15dc74880c9944775448304b263d191c6077f",
+	},
+	{
+		id: "celo",
+		name: "Celo",
+		wrappedTokenId: "0x471ece3750da237f93b8e339c536989b8978a438",
+	},
+	{
+		id: "era",
+		name: "zkSync Era",
+		wrappedTokenId: "0x5aea5775959fbc2557cc8789bc1bf90a239d9a91",
+	},
+	{
+		id: "tomb",
+		name: "TOMB Chain",
+		wrappedTokenId: "0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000",
+	},
+	{
+		id: "kroma",
+		name: "Kroma",
+		wrappedTokenId: "0x4200000000000000000000000000000000000001",
+	},
+	{
+		id: "zkfair",
+		name: "ZKFair",
+		wrappedTokenId: "0xd33db7ec50a98164cc865dfaa64666906d79319c",
+	},
+	{
+		id: "base",
+		name: "Base",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "sbch",
+		name: "SmartBch",
+		wrappedTokenId: "0x3743ec0673453e5009310c727ba4eaf7b3a1cc04",
+	},
+	{
+		id: "ron",
+		name: "Ronin",
+		wrappedTokenId: "0xe514d9deb7966c8be0ca922de8a064264ea6bcd4",
+	},
+	{
+		id: "cfx",
+		name: "Conflux",
+		wrappedTokenId: "0x14b2d3bc65e74dae1030eafd8ac30c533c976a9b",
+	},
+	{ id: "alot", name: "Dexalot", wrappedTokenId: "" },
+	{
+		id: "rsk",
+		name: "Rootstock",
+		wrappedTokenId: "0x542fda317318ebf1d3deaf76e0b632741a7e677d",
+	},
+	{
+		id: "sx",
+		name: "SX",
+		wrappedTokenId: "0xaa99be3356a11ee92c3f099bd7a038399633566f",
+	},
+	{
+		id: "karak",
+		name: "Karak",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "fon",
+		name: "FON Chain",
+		wrappedTokenId: "0xb582fd9d0d5c3515eeb6b02ff2d6ee0b6e45e7a7",
+	},
+	{
+		id: "tenet",
+		name: "Tenet",
+		wrappedTokenId: "0xd6cb8a253e12893b0cf39ca78f7d858652cca1fe",
+	},
+	{
+		id: "eth",
+		name: "Ethereum",
+		wrappedTokenId: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+	},
+	{
+		id: "op",
+		name: "Optimism",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "zeta",
+		name: "ZetaChain",
+		wrappedTokenId: "0x5f0b1a82749cb4e2278ec87f8bf6b618dc71a8bf",
+	},
+	{ id: "dym", name: "Dymension", wrappedTokenId: "" },
+	{ id: "hubble", name: "Hubble", wrappedTokenId: "" },
+	{
+		id: "sgb",
+		name: "Songbird",
+		wrappedTokenId: "0x02f0826ef6ad107cfc861152b32b52fd11bab9ed",
+	},
+	{
+		id: "savm",
+		name: "SatoshiVM Alpha",
+		wrappedTokenId: "0x5db252ead05c54b08a83414adcabf46eaa9e0337",
+	},
+	{
+		id: "evmos",
+		name: "EvmOS",
+		wrappedTokenId: "0xd4949664cd82660aae99bedc034a0dea8a0bd517",
+	},
+	{
+		id: "eon",
+		name: "Horizen EON",
+		wrappedTokenId: "0xf5cb8652a84329a2016a386206761f455bcedab6",
+	},
+	{
+		id: "eos",
+		name: "EOS EVM",
+		wrappedTokenId: "0xc00592aa41d32d137dc480d9f6d0df19b860104f",
+	},
+	{
+		id: "scrl",
+		name: "Scroll",
+		wrappedTokenId: "0x5300000000000000000000000000000000000004",
+	},
+	{
+		id: "linea",
+		name: "Linea",
+		wrappedTokenId: "0xe5d7c2a44ffddf6b295a15c148167daaaf5cf34f",
+	},
+	{
+		id: "opbnb",
+		name: "opBNB",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "xdai",
+		name: "Gnosis Chain",
+		wrappedTokenId: "0xe91d153e0b41518a2ce8dd3d7944fa863463a97d",
+	},
+	{
+		id: "rollux",
+		name: "Rollux",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "aurora",
+		name: "Aurora",
+		wrappedTokenId: "0xc9bdeed33cd01541e1eed10f90519d2c06fe3feb",
+	},
+	{
+		id: "kava",
+		name: "Kava",
+		wrappedTokenId: "0xc86c7c0efbd6a49b35e8714c5f59d99de09a225b",
+	},
+	{
+		id: "aze",
+		name: "Astar zkEVM",
+		wrappedTokenId: "0xe9cc37904875b459fa5d0fe37680d36f1ed55e38",
+	},
+	{
+		id: "ckb",
+		name: "Godwoken",
+		wrappedTokenId: "0xc296f806d15e97243a08334256c705ba5c5754cd",
+	},
+	{
+		id: "nova",
+		name: "Arbitrum Nova",
+		wrappedTokenId: "0x722e8bdd2ce80a4422e880164f2079488e115365",
+	},
+	{
+		id: "hmy",
+		name: "Harmony",
+		wrappedTokenId: "0xcf664087a5bb0237a0bad6742852ec6c8d69a27a",
+	},
+	{
+		id: "canto",
+		name: "Canto",
+		wrappedTokenId: "0x826551890dc65655a0aceca109ab11abdbd7a07b",
+	},
+	{
+		id: "kcc",
+		name: "KCC",
+		wrappedTokenId: "0x4446fc4eb47f2f6586f9faab68b3498f86c07521",
+	},
+	{
+		id: "bevm",
+		name: "BEVM",
+		wrappedTokenId: "0xb5136feba197f5ff4b765e5b50c74db717796dcd",
+	},
+	{
+		id: "ulx",
+		name: "Ultron",
+		wrappedTokenId: "0x3a4f06431457de873b588846d139ec0d86275d54",
+	},
+	{
+		id: "matic",
+		name: "Polygon",
+		wrappedTokenId: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+	},
+	{
+		id: "mada",
+		name: "Milkomeda C1",
+		wrappedTokenId: "0xae83571000af4499798d1e3b0fa0070eb3a3e3f9",
+	},
+	{
+		id: "movr",
+		name: "Moonriver",
+		wrappedTokenId: "0x98878b06940ae243284ca214f92bb71a2b032b8a",
+	},
+	{
+		id: "cro",
+		name: "Cronos",
+		wrappedTokenId: "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23",
+	},
+	{
+		id: "boba",
+		name: "Boba",
+		wrappedTokenId: "0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000",
+	},
+	{
+		id: "platon",
+		name: "PlatON",
+		wrappedTokenId: "0x02406d561069cbed27ef8ea20afd41779a90e2bf",
+	},
+	{
+		id: "manta",
+		name: "Manta Pacific",
+		wrappedTokenId: "0x0dc808adce2099a9f62aa87d9670745aba741746",
+	},
+	{
+		id: "pze",
+		name: "Polygon zkEVM",
+		wrappedTokenId: "0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9",
+	},
+	{
+		id: "blast",
+		name: "Blast",
+		wrappedTokenId: "0x4300000000000000000000000000000000000004",
+	},
+	{
+		id: "fsn",
+		name: "Fusion",
+		wrappedTokenId: "0x0c05c5710af74d36b4d3bd5460475c20ceca8fe3",
+	},
+	{
+		id: "iotx",
+		name: "IoTeX",
+		wrappedTokenId: "0xa00744882684c3e4747faefd68d283ea44099d03",
+	},
+	{
+		id: "beam",
+		name: "Beam",
+		wrappedTokenId: "0xd51bfa777609213a653a2cd067c9a0132a2d316a",
+	},
+	{
+		id: "lumio",
+		name: "SuperLumio",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "bsc",
+		name: "BNB Chain",
+		wrappedTokenId: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
+	},
+	{
+		id: "map",
+		name: "MAP Protocol",
+		wrappedTokenId: "0x13cb04d4a5dfb6398fc5ab005a6c84337256ee23",
+	},
+	{
+		id: "metis",
+		name: "Metis",
+		wrappedTokenId: "0x75cb093e4d61d2a2e65d8e0bbb01de8d89b53481",
+	},
+	{
+		id: "okt",
+		name: "OKC",
+		wrappedTokenId: "0x8f8526dbfd6e38e3d8307702ca8469bae6c56c15",
+	},
+	{
+		id: "dfk",
+		name: "DFK",
+		wrappedTokenId: "0xccb93dabd71c8dad03fc4ce5559dc3d89f67a260",
+	},
+	{
+		id: "oas",
+		name: "Oasys",
+		wrappedTokenId: "0x5200000000000000000000000000000000000001",
+	},
+	{
+		id: "shib",
+		name: "Shibarium",
+		wrappedTokenId: "0x213c25900f365f1be338df478cd82bef7fd43f85",
+	},
+	{
+		id: "doge",
+		name: "Dogechain",
+		wrappedTokenId: "0xb7ddc6414bf4f5515b52d8bdd69973ae205ff101",
+	},
+	{
+		id: "mtr",
+		name: "Meter",
+		wrappedTokenId: "0x160361ce13ec33c993b5cca8f62b6864943eb083",
+	},
+	{
+		id: "tlos",
+		name: "Telos",
+		wrappedTokenId: "0xd102ce6a4db07d247fcc28f366a623df0938ca9e",
+	},
+	{
+		id: "zora",
+		name: "Zora",
+		wrappedTokenId: "0x4200000000000000000000000000000000000006",
+	},
+	{
+		id: "etc",
+		name: "Ethereum Classic",
+		wrappedTokenId: "0x82a618305706b14e7bcf2592d4b9324a366b6dad",
+	},
+	{
+		id: "brise",
+		name: "Bitgert",
+		wrappedTokenId: "0x0eb9036cbe0f052386f36170c6b07ef0a0e3f710",
+	},
+	{
+		id: "astar",
+		name: "Astar",
+		wrappedTokenId: "0xaeaaf0e2c81af264101b9129c00f4440ccf0f720",
+	},
+];
